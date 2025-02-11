2b78efbb35c2bc8ba44c30eb36de60d50c0bd061
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-11 07:47:00 +0330

    Update insert_data.py

--- core/todo/management/commands/insert_data.py
+++ core/todo/management/commands/insert_data.py
@@ -4,15 +4,16 @@ from todo.models import Task
 from django.contrib.auth.models import User
 import random
 
+
 class Command(BaseCommand):
-    help = "Inserting dummy data" 
+    help = "Inserting dummy data"
     fake = Faker()
+
     def handle(self, *args, **options):
-        
 
         user, created = User.objects.get_or_create(username=self.fake.user_name())
         if created:
-            user.set_password('pass/@1234567')  # set hashed password
+            user.set_password("pass/@1234567")  # set hashed password
             user.first_name = self.fake.first_name()
             user.last_name = self.fake.last_name()
             user.email = self.fake.email()
@@ -20,12 +21,16 @@ class Command(BaseCommand):
 
         # generate a list of 5 task objects (but not yet saved in the database)
         tasks = [
-            Task(user=user, title=self.fake.word().title(), complete=random.choice((True, False)))
+            Task(
+                user=user,
+                title=self.fake.word().title(),
+                complete=random.choice((True, False)),
+            )
             for _ in range(5)
-        ] 
+        ]
 
         # insert all tasks into the database with a single query (optimized)
         Task.objects.bulk_create(tasks)
 
         # print success message in green color
-        self.stdout.write(self.style.SUCCESS('Dummy data inserted successfully!'))
+        self.stdout.write(self.style.SUCCESS("Dummy data inserted successfully!"))

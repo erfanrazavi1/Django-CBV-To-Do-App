28197380f94f7f568fa302dbf2298d915dfb7c1d
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-14 00:22:33 +0330

    Add Celery Beat to schedule task deletion every 10 minutes
    Added a new Celery Beat service in docker-compose.yml
    Created a scheduled task to delete completed tasks every 10 minutes
    Implemented the delete_completed_tasks function in todo/tasks.py
    Configured Celery Beat scheduling in celery.py

--- core/core/celery.py
+++ core/core/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -15,3 +16,11 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'delete-completed-tasks-every-10-minutes': {
+        'task': 'todo.tasks.delete_completed_tasks',
+        'schedule': crontab(minute='*/10'),
+    },
+}
--- core/core/settings.py
+++ core/core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "drf_yasg",
     "mail_templated",
     "corsheaders",
+    "django_celery_beat",
 ]
 
 MIDDLEWARE = [

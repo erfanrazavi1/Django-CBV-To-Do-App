9c33f8b8f6f572800c78c8df2ea11e88d428ba2f
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-15 10:26:18 +0330

    Update database settings to use PostgreSQL and add environment variables
    - Replaced SQLite database configuration with PostgreSQL settings in Django project.
    - Added PostgreSQL environment variables for database connection (DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT).
    - Installed psycopg2-binary and libpq-dev for PostgreSQL support.
    - Configured Docker container for PostgreSQL and set environment variables.
    - Updated `DATABASES` settings in Django to use PostgreSQL.

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,13 @@ ENV PYTHONUNBUFFERED=1
 
 WORKDIR /app
 
+RUN apt-get update && apt-get install -y libpq-dev
+
 COPY requirements.txt /app/
 
 RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
+
+RUN pip3 install psycopg2-binary
 
 COPY ./core /app
--- core/core/settings.py
+++ core/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,13 +92,17 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'todo_django_db'),
+        'USER': os.getenv('POSTGRES_USER', 'erfan'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'ZxCv@#1234567'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
